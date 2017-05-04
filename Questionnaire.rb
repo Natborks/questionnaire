5af74ee5659b85b4f6cf3585c81ccee0ad374c59
natborks <natborksdeutsch@yahoo.com>
2017-05-04 21:18:11 +0000

    add group and ccalculate functions

--- Questionnaire.rb
+++ Questionnaire.rb
@@ -4,12 +4,34 @@ class Analysis
 	attr_reader :result
 
 	def read_csv
-		CSV.foreach('Delanyo (1).csv')	do |row|
-			row.inspect #if row[0][0] > 1
-		end	
+		@result = CSV.open "Delanyo (1).csv"	
+		return result
 	end
+
+	def csv(result)
+			zero = Array.new
+			#@result = CSV.open "Delanyo (1).csv"	
+			result.each_with_index do |row,inx|	
+					 zero[inx] = 0			
+					 row.each_with_index do |column,i|						
+						zero[inx] += column.to_i	if i > 3		
+					end
+			end
+			return zero
+	end
+
+	def groupings(result)
+		uni = Array.new
+		result.each do |row|
+			uni << row if row[2] == "University"
+		end	
+		return uni
+	end
+
 end
 
 new_analysis = Analysis.new
-puts new_analysis.read_csv
+result = new_analysis.read_csv
+uni = new_analysis.groupings(result)
+puts new_analysis.csv(uni)
 
