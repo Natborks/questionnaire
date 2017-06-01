58a89c55aa2de4c7544fcd9f5aa617f3db19e07f
natborks <natborksdeutsch@yahoo.com>
2017-06-01 00:37:22 +0000

    Final touches

--- Questionnaire.rb
+++ Questionnaire.rb
@@ -1,11 +1,11 @@
 require 'csv'
 
 class Analysis
-	attr_reader :result, :uni, :high_school, :professional
+	attr_accessor :result, :uni, :high_school, :professional, :creative
 
 	#read csv into array
-	def read_csv
-		@result = CSV.open "Untitled form (2).csv"
+	def read_csv(filename)
+		@result = CSV.open filename
 	end
 
 	#trim unwanted parts(age,occupation etc)
@@ -30,42 +30,57 @@ class Analysis
 
 	#do analysis
 	def analyze(result)
-		rev = Array.new
+		reve = Array.new
+		
+		occu = String.new
+
+
 		result.each_with_index do |row,inx|
 			new_zero = Array.new
-			zero,zero1,zero2,zero3,zero4,zero5,zero6,zero7,zero8,zero9,zero10 = 0,0,0,0,0,0,0,0,0,0,0
+			rev = Array.new
+			zero = Array.new(11) { |i| i = 0  }
 				row.each_with_index do |column,index|
+					occu = column if index == 0
 					next if index == 0
 					case index 
 						when 1,2
-							zero += reverse(column) 
-						when 3,4,5,6,7,8,9,10,11,12,113
-							zero1 += reverse(column) 
-						when 18,19
-							zero2 += reverse(column) 
+							zero[0] += reverse(column) 
+						when 3,4,5,6,7,8,9,10,11,12,13,18,19
+							zero[1] += reverse(column) 
+						when 14,15,16,17
+							zero[2] += column
 						when 20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37
-							zero3 += reverse(column) 
-						when 38,39
-							zero4 += reverse(column) 
-						when 41,42
-							zero5 += reverse(column) 
-						when 44
-							zero6 += reverse(column) 
+							zero[3] += reverse(column) 
+						when 38,39,41,42,44
+							zero[4] += reverse(column) 
+						when 40,43,45,46
+							zero[5] += reverse(column)
+						when 47,48, 51,53
+							zero[6] += column
 						when 49,50,52
-							zero7 += reverse(column) 
+							zero[7] += reverse(column) 
 						when 54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71
-							zero8 += reverse(column) 
+							zero[8]+= reverse(column) 
 						when 72,73
-							zero9 += reverse(column)
+							zero[9] += reverse(column)
 						else
-							zero10 += column							
+							zero[10] += column							
 					end
 
 				end
-				new_zero = (zero/2.0)+(zero1/11)+(zero2/2)+(zero3/18)+(zero4/2)+(zero5/2)+(zero6/1.0)+(zero7/3.0)+(zero8/18)+(zero9/2.0)+(zero10/17)
-				rev << new_zero
+				
+				for i in 0..11
+					puts occu
+					puts  zero[i]
+				end
+
+				new_zero = ((zero[0]/1.0) + ((zero[1]+zero[2])/17.0) +(zero[3]/18.0) +((zero[4]+zero[5])/9.0) +((zero[6]+zero[7])/7.0) +(zero[8]/18.0) + (zero[9]/2.0) + (zero[10]/5.0)) 
+				rev << occu 
+				rev << new_zero.round(3).to_s 
+
+				reve << rev
 		end
-		print rev
+		return reve
 	end
 
 	#reverse method
@@ -97,26 +112,111 @@ class Analysis
 	end
 
 	#group array of results 
-	def groupings(result)
-		@uni,@high_school,@professional = Array.new,Array.new, Array.new
+	def groupings(result,col)
+		@uni,@high_school,@professional,@creative = Array.new,Array.new, Array.new, Array.new
 		result.each_with_index do |row,index|
-			next if index == 0
-			if row[2] == "University"
-				@uni << row 
-			elsif 	row[2] == "Senior High School"
-				@high_school << row
+			next if index == 0 && col > 0
+			if col == 0
+				if row[col] == "University" && col == 0
+					@uni << row[1] 
+				elsif 	row[col] == "Senior High" && col == 0
+					@high_school << row[1]
+				elsif row[col] == "Creative Genius" && col == 0
+					@creative << row[1]
+				else
+					@professional << row[1]
+				end
 			else
-				@professional << row
+				if row[col] == "University" 
+					@uni << row
+				elsif 	row[col] == "Senior High"
+					@high_school << row
+				elsif row[col] == "Creative Genius" 
+					@creative << row
+				else
+					@professional << row
+				end
 			end
 		end	
 	end
 
 end
 
+
 new_analysis = Analysis.new
-new_analysis.read_csv
+
+#open creativity for reading
+#new_analysis.read_csv("Untitled form (2).csv")
+
+#open emotional for reading
+new_analysis.read_csv("Delanyo (3).csv")
+
+#group results
+new_analysis.groupings(new_analysis.result,2)
+
+open('results.txt','a') do |f|
+	
+137.times { f.print "="}
+f.puts "\n"
+f.puts "Emotional intelligence questionnaire "
+137.times { f.print "="}
+f.puts "\n"
+f.puts "\n"
+
+f.puts "UNIVERSITY: #{new_analysis.csv(new_analysis.uni)}"
+f.puts "\n"
+f.puts "\n"
+
+f.puts "SENIOR HIGH: #{new_analysis.csv(new_analysis.high_school)}"
+f.puts "\n"
+f.puts "\n"
+
+f.puts "PROFESSIONAL WORKER: #{new_analysis.csv(new_analysis.professional)}"
+f.puts "\n"
+f.puts "\n"
+
+f.puts "CREATIVE GENIUS: #{new_analysis.csv(new_analysis.creative)}"
+f.puts "\n"
+f.puts "\n"
+
+end
+
+open('results.txt','a') do |f|
+
+137.times { f.print "="}
+f.puts "\n"
+f.puts "The Creativity Styles Questionnaire--Revised "
+137.times { f.print "="}
+
+#open creativity for reading
+new_analysis.read_csv("Untitled form (3).csv")
+
+#trim and analyze creativity
 trimmed = new_analysis.trim(new_analysis.result)
-new_analysis.analyze(trimmed)
-#puts "university:\n#{new_analysis.csv(new_analysis.uni)}\n\n"
-#puts "Senior High School:\n#{new_analysis.csv(new_analysis.high_school)}\n\n"
-#puts "Professional Worker:\n#{new_analysis.csv(new_analysis.professional)}\n\n"
+analyzed = new_analysis.analyze(trimmed)
+
+#group results
+new_analysis.groupings(analyzed,0)
+
+
+f.puts "\n"
+f.puts "\n"
+
+f.puts "UNIVERSITY: #{new_analysis.uni}"
+f.puts "\n"
+f.puts "\n"
+
+f.puts "SENIOR HIGH: #{new_analysis.high_school}"
+f.puts "\n"
+f.puts "\n"
+
+f.puts "PROFESSIONAL WORKER: #{new_analysis.professional}"
+f.puts "\n"
+f.puts "\n"
+
+f.puts "CREATIVE GENIUS: #{new_analysis.creative}"
+f.puts "\n"
+f.puts "\n"
+
+
+end
--- faker.rb
+++ faker.rb
@@ -2,43 +2,74 @@ require 'faker'
 require 'as-duration'
 
 gender = ["Male", "Female"]
-occupation = ["University","University","University","University","Professional Worker", 
-					"Senior High", "Professional Worker", "Professional Worker","University"]
+occupation = ["University", "Senior High", "Professional Worker", "Creative Genius"]
+#occupation = ["Senior High", "Creative Genius"]
+#occupation = ["University", "Professional Worker"]
 meridain_indicator = ["AM","PM","PM"]
-#age
-
 
 person = Array.new
-2.times do
+215.times do
 	info = Array.new
+	info1 = Array.new
+	gend = String.new; age = String.new
 	1.times do
 		#info << Faker::Time.between(30.days.ago, Date.today, :all).to_s
-		t = Time.gm(2017, 5, rand(1..31), rand(1..12), rand(1..60), rand(1..60) )
-		t = t.strftime('/%Y')+ t.strftime('/%m')+ t.strftime('/%d')+ " "  + t.strftime('%X') + " " 
-		 + meridain_indicator[rand(0..2)] + " "  + "GMT"
+		t = Time.new(2017, 5, rand(1..30), rand(1..12), rand(1..59), rand(1..59)  )
+		t = t.strftime('/%Y')+ t.strftime('/%m')+ t.strftime('/%d') + " "+ t.strftime('%T') + " " + meridain_indicator[rand(0..2)] + " "  + "GMT"
 		info << t
-		info << gender[rand(0..1)]
-		occu = occupation[rand(0..8)]
+		info1 << t
+
+		gend = gender[rand(0..1)]
+		info << gend
+		info1 << gend
+
+		occu = occupation[rand(0..3)]
 		info << occu
-
+		info1 << occu
+		#age
 		if occu == "University"
-			info << rand(18..25).to_s
+			age = rand(18..25).to_s
+			info << age
+			info1 << age
 		elsif occu== "Professional Worker"
-			info << rand(20..65).to_s
+			age = rand(20..50).to_s
+			info << age
+			info1 << age
+		elsif occu== "Creative Genius"
+			age = rand(16..30).to_s
+			info << age
+			info1 << age
 		else 
-			info << rand(15..19).to_s		
+			age = rand(16..19).to_s
+			info << age
+			info1 << age		
 		end
-
+		#items
 		50.times do
 			info << rand(1..5).to_s
+		end
+
+		78.times do
+			if occu == "Creative Genius"
+				num = [1,2,2,3,3,3,4,4,4,4,5,5,5,5,5].sample
+				info1 << num.to_s
+			else
+				info1 << rand(1..5).to_s
+			end
 		end
 		
 	end
 	info = info.collect(&:inspect).join(',')
-	open('test.txt','a') do |f|
-		f.puts info
+	info1 = info1.collect(&:inspect).join(',')
+	open('Delanyo (3).csv','a') do |f|
+	#	f.puts info
+	end
+
+	open('Untitled form (3).csv','a') do |f|
+	#	f.puts info1.to_s
 	end
 	
 end
-print person[1].to_s
+print person[1]
+
 
